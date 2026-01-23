b497177e7796cf03126d1a3c08f5bafefe19da0c
Cleudice Santos <cleudice.ms@gmail.com>
2026-01-22 21:49:21 -0300

    Update Repository

--- src/Cms.BuildingBlocks.Application/Core/Persistence/IRepository.cs
+++ src/Cms.BuildingBlocks.Application/Core/Persistence/IRepository.cs
@@ -2,12 +2,12 @@ using Cms.BuildingBlocks.Domain.Abstractions;
 
 namespace Cms.BuildingBlocks.Application.Core.Persistence;
 
-public interface IRepository<TAggregate>
-    where TAggregate
-        : AggregateRoot<IEntityId>
+public interface IRepository<TAggregate, TId>
+    where TAggregate : AggregateRoot<TId>
+    where TId : IEntityId
 {
     Task AddAsync(TAggregate aggregate, CancellationToken ct = default);
     Task UpdateAsync(TAggregate aggregate, CancellationToken ct = default);
-    Task<TAggregate?> GetByIdAsync(IEntityId id, CancellationToken ct = default);
+    Task<TAggregate?> GetByIdAsync(TId id, CancellationToken ct = default);
     Task<IEnumerable<TAggregate>> ListAsync(CancellationToken ct = default);
 }
